c0000e621ec59abcf1de49747a474625cac8b9fe
Shiraz Butt <shiraz.b@icloud.com>
2018-01-04 21:01:52 +0000

    made initial board class

---
+++ src/main/java/com/shirazb/app/Board.java
@@ -0,0 +1,13 @@
+package com.shirazb.app;
+
+public class Board {
+    private int[][] board = new int[3][3];
+
+    public boolean isGameOver() {
+        return true;
+    }
+
+    public void playMove(Move m) {
+
+    }
+}
